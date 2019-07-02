c8450c923126069e554b05a8bb9cfdc3581fdc4e
wisnuramadhani <52442195+wisnuramadhani@users.noreply.github.com>
2019-07-02 15:35:44 +0700

    contoh ngirim file

---
+++ 1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php  
+$hari = array(0 =>"Senin","Selasa","rabu","kamis","jumat","Sabtu","minggu" );
+
+
+foreach ($hari as $key => $value) {
+	print $key.".".$value."<br/>";
+}
+?>
+</body>
+</html>
